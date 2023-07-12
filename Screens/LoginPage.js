673e36fedd3818141ccc502e29010593c63e6c15
priyankara97 <profront0tohero@gmail.com>
2023-07-12 02:36:54 -0700

    Login page finished

--- Screens/LoginPage.js
+++ Screens/LoginPage.js
@@ -1,40 +1,65 @@
-import { View, Text, StyleSheet, Image, FlatList } from 'react-native'
+import { View, Text, StyleSheet, Image, FlatList, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { Button, TextInput } from 'react-native-paper'
 
 const LoginPage = () => {
- 
+
 
   return (
-    <View style={styles.container}>
+    <View style={styles.container} >
 
 
-      <View style={{ flex: 4, backgroundColor: "brown", padding: 15 }}>
+      <View style={{ flex: 2, padding: 15, alignContent: "center", alignItems: "center" }}>
         <Image
 
           source={require('../assests/logo.jpeg')}
           style={styles.img}
         />
       </View >
-      <View style={{ flex: 3, backgroundColor: "yellow" ,padding:15}}>
-      <TextInput
-      label="Login"
-     
-      mode='flat'
-     
-    />
-    <Text>{'\n'}</Text>
-      <TextInput
-      label="password"
-     
-      mode='flat'
-      secureTextEntry={true}
-    />
+      <View style={{ flex: 3, padding: 15 }}>
+        <TextInput
+          label="Username"
+
+          mode='flat'
+          style={{ backgroundColor: "white", marginBottom: 5 }}
+        />
+
+        <TextInput
+          label="password"
+
+          mode='flat'
+          secureTextEntry={true}
+          style={{ backgroundColor: "white", marginTop: 6 }}
+        />
+
+        <Text style={{ textDecorationLine: "underline", marginTop: 8, textAlign: "right" }}>Forgot Password</Text>
       </View>
-      <View style={{ flex: 6 }}>
-      <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
-    Press me
-  </Button>
+      <View style={{ flex: 6, padding: 15, marginTop: 50 }}>
+        <Text>
+          <TouchableOpacity style={{ height: 50 }}>
+            <Text style={{ fontSize: 16, color: "black", backgroundColor: "white", borderColor: 'blue', borderWidth: 1, padding: 10, width: 280, height: 40, textAlignVertical: "center", textAlign: "center" }}>Login</Text>
+          </TouchableOpacity   >
+          <View style={{ flex: 1, textAlign: "right" }}>
+            <Text style={{ marginTop: 8 }} >Or</Text>
+          </View>
+
+          <Text>{'\n'}</Text>
+          <Text>{'\n'}</Text>
+
+          <Text>{'\n'}</Text>
+
+          <TouchableOpacity style={{ height: 50 }}>
+            <Text style={{ fontSize: 16, color: "black", backgroundColor: "white", borderColor: 'blue', borderWidth: 1, padding: 10, width: 280, height: 40, textAlignVertical: "center", textAlign: "center" }}>Login with Facebook</Text>
+          </TouchableOpacity>
+
+          <Text>{'\n'}</Text>
+          <Text>{'\n'}</Text>
+
+          <TouchableOpacity style={{ height: 50 }}>
+            <Text style={{ fontSize: 16, color: "black", backgroundColor: "white", borderColor: 'blue', borderWidth: 1, padding: 10, width: 280, height: 40, textAlignVertical: "center", textAlign: "center" }}>Register</Text>
+          </TouchableOpacity>
+
+        </Text>
       </View>
 
     </View>
@@ -46,18 +71,22 @@ export default LoginPage
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    padding: 10,
+    borderColor: "balck",
+    borderWidth: 1,
+    margin: 12,
+    borderRadius: 10
   },
 
   img: {
-    width: 130,
-    height: 130,
+    width: 80,
+    height: 80,
+    flexDirection: 'column',
     justifyContent: 'center',
-    alignContent: 'center',
-    alignItems: 'center',
 
+  },
 
-  }
 })
 
 
