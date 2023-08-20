8a2f97ce900de7fa11167ba472060ca94dd64d47
priyankara97 <profront0tohero@gmail.com>
2023-08-19 21:07:41 -0700

    loadData

--- Components/LoadData.js
+++ Components/LoadData.js
@@ -1,11 +1,18 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { FlatList, Text, View } from 'react-native'
-import { Button } from 'react-native-paper'
+import { Button, TextInput } from 'react-native-paper'
 
 const LoadData = () => {
 
 
   const [data,setData]=useState([]);
+  const [name,setName]=useState('');
+
+useEffect (()=>{
+  // AllData();
+  console.log("hello")
+},[])
+
 const AllData =() =>{
 
   
@@ -21,18 +28,19 @@ const AllData =() =>{
 <Text>Data</Text>
 
 <Button mode='contained' onPress={AllData} >Dataload</Button>
-
+<TextInput mode='outlined' placeholder='enter your email' onChangeText={(t)=>{setName(t)}}></TextInput>
 <FlatList
         data={data}
         renderItem={({item}) => {return(
           <View>
-            <Text>{item.body}</Text>
+            <Text>{item.title}</Text>
           </View>
         )}}
         keyExtractor={item => item.id}
       />
   </View>
-  ) 
+  )
 }
 
 export default LoadData
+// 1.40 minutes wathed 
